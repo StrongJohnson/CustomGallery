2077b51d81274c583cabd9cce7bf593834fc6523
StrongJohnson <nkwondev@gmail.com>
2012-03-22 17:21:15 +0900

    First commit. -v.1.0

---
+++ CustomGallery/src/com/personal/customgallery/CustomGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.personal.customgallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomGalleryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
