e656c9da4951b9a504623670d22a0431cd2f9a86
StrongJohnson <nkwondev@gmail.com>
2012-03-22 17:58:34 +0900

    first commit

--- CustomGallery/src/com/personal/customgallery/CustomGalleryActivity.java
+++ CustomGallery/src/com/personal/customgallery/CustomGalleryActivity.java
@@ -2,6 +2,7 @@ package com.personal.customgallery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class CustomGalleryActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,7 @@ public class CustomGalleryActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Log.i("tagg","first commit!!!");
     }
 }
